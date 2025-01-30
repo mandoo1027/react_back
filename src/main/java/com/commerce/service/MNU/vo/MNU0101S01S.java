2b2b1cf28a312ff4ee32fd236a61282f5237838a
김경일 <mandoo1027@gmail.com>
2025-01-30 22:32:25 +0900

    [경일] 메뉴 조회

--- src/main/java/com/commerce/service/MNU/vo/MNU0101S01S.java
+++ src/main/java/com/commerce/service/MNU/vo/MNU0101S01S.java
@@ -8,4 +8,9 @@ import org.springframework.stereotype.Component;
 public class MNU0101S01S {
     private String type; // 메뉴타입
     private String sysDivCd;
+    private String menuCode;
+    private String menuName;
+    private String use_yn;
+    private String menuScrDev;
+
 }
